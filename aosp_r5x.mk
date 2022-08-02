643a6e324df855a5e917826d72d7c7f6a17200f9
JaswantTeja <sunnyteja201@gmail.com>
2022-08-02 14:39:50 +0530

    r5x : #GetPixelized By PixelPlusUI

--- aosp_r5x.mk
+++ aosp_r5x.mk
@@ -9,12 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common PixelPlusUI stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# PixelPlusUI Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+IS_PHONE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
 #TARGET_GAPPS_ARCH := arm64
 #TARGET_INCLUDE_LIVE_WALLPAPERS := false
